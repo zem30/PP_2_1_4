c395b8ffce1cb1633f4fed58ec96784816b7f7c3
Alexe1Kal1n1n <kalinin.aleks199813@yandex.ru>
2023-11-14 19:03:04 +0400

    Task PP_2_1_4 done

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,19 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
+    @Autowired
+    private final Duck5 duck;
+
+    public Rabbit4(Duck5 duck) {
+        this.duck = duck;
+    }
 
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + duck.toString();
     }
 }
